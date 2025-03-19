e5fc37c7db5b4610a7dd51e3ac06c0c2f0f6a3f1
ferematsuzaki3232 <fereple.y.matsuzaki@gmail.com>
2025-03-19 12:21:09 +0900

    kadai_011　アップロードしました

---
+++ kadai_011/foreach.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>12章課題</title>
+</head>
+<body>
+    <p>
+    <?php
+        $score = [
+            '名前' => '玉ねぎ',
+            '値段' => 200,
+            '産地' => '北海道'
+        ];
+        //  配列$user_namesの要素を1つずつ順番に出力する
+        foreach ($score as $score) {
+            echo $score . '<br>';
+        }
+        }
+     ?>
+    </p>
+</body>
+</html>
