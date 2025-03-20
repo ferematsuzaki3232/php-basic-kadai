12d0fce7fdfb117216bec28b962e71362eb92dfc
ferematsuzaki3232 <fereple.y.matsuzaki@gmail.com>
2025-03-20 12:04:15 +0900

    kadai_sort　アップロード

---
+++ kadai_sort/sort.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+<!-- http://localhost:8888/kadai_sort/sort.php -->
+<body>
+    <p>
+        <?php
+        // ここにコードを書いていく
+        $nums = [15, 4, 18, 23, 10 ];
+        ?>
+    </p>
+</body>
+
+</html>
