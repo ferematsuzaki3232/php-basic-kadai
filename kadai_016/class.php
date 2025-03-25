1b1c42ecbe5a7cfbfb900d75c01195d1399233a4
ferematsuzaki3232 <fereple.y.matsuzaki@gmail.com>
2025-03-25 12:41:36 +0900

    kadai_016　更新しました

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -9,33 +9,43 @@
   <p>
   <?php
   class Food{
-    public $name;
-    public $price;
+    private $name;
+    private $price;
 
-    public function __construct($name, $price) {
+    public function __construct(string $name, int $price) {
       $this->name = $name;
       $this->price = $price;
       }
     public function show_price(){
-        echo $this->name , '<br>';
+        echo $this->price .'<br>';
       }
   }
 $food = new Food('potato', 250);
-  class Animal{
-    public $name;
-    public $height;
-    public $weight;
 
-    public function __construct($name, $height, $weight) {
+  class Animal{
+    private $name;
+    private $height;
+    private $weight;
+
+    public function __construct(string $name, int $height, int $weight) {
       $this->name = $name;
       $this->height = $height;
       $this->weight = $weight;
   }
+    public function show_height(){
+       echo $this->height;
+    } 
   }
 $animal = new Animal('dog', 60, 5000);
 
-  print_r($price);
+
+  print_r($food);
+  echo '<br>';
   print_r($animal);
+
+  echo '<br>';
+  $food->show_price();
+  $animal->show_height();
   ?>
   </p>
 </body>
