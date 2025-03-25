783843b0c6e2e17d05feca57eaaa19ee96c41922
ferematsuzaki3232 <fereple.y.matsuzaki@gmail.com>
2025-03-25 12:42:18 +0900

    kadai_sort　更新しました

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -11,6 +11,18 @@
         <?php
         // ここにコードを書いていく
         $nums = [15, 4, 18, 23, 10 ];
+        function sort_2way($array,$order){
+            if($order){
+                sort($array);
+            }else{
+                rsort($array);
+            }
+            foreach($array as $num){
+                echo $num ."<br>";
+            }
+        }
+        sort_2way($nums,true);
+        sort_2way($nums,false);
         ?>
     </p>
 </body>
